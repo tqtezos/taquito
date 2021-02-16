80d65f6075234608be2da179c5d758ada1326aad
Philip Diaz <philip@tqgroup.io>
2021-02-16 15:33:21 -0500

    feat(tzip16): Add protocol option to IpfsHttpHandler constructor

--- packages/taquito-tzip16/src/handlers/ipfs-handler.ts
+++ packages/taquito-tzip16/src/handlers/ipfs-handler.ts
@@ -1,22 +1,29 @@
-import { HttpBackend } from "@taquito/http-utils";
-import { ContractAbstraction, ContractProvider, Wallet, Context } from "@taquito/taquito";
-import { Handler, Tzip16Uri } from "../metadata-provider";
+import { HttpBackend } from '@taquito/http-utils';
+import { ContractAbstraction, ContractProvider, Wallet, Context } from '@taquito/taquito';
+import { Handler, Tzip16Uri } from '../metadata-provider';
 
 export class IpfsHttpHandler implements Handler {
-    private _ipfsGateway: string;
-    private _httpBackend = new HttpBackend();
+  private _ipfsGateway: string;
+  private _gatewayProtocol: string;
+  private _httpBackend = new HttpBackend();
 
-    constructor(ipfsGatheway?:string){
-        this._ipfsGateway = ipfsGatheway? ipfsGatheway: 'ipfs.io';
-    }
+  constructor(ipfsGatheway?: string, gatewayProtocol?: string) {
+    this._ipfsGateway = ipfsGatheway ? ipfsGatheway : 'ipfs.io';
+    this._gatewayProtocol = gatewayProtocol ? gatewayProtocol : 'https';
+  }
 
-    async getMetadata(_contractAbstraction: ContractAbstraction<ContractProvider | Wallet>, { location }: Tzip16Uri, _context: Context): Promise<string> {
-        return this._httpBackend.createRequest<string>({
-            url: `https://${this._ipfsGateway}/ipfs/${location.substring(2)}/`,
-            method: 'GET',
-            headers: {'Content-Type': 'text/plain'},
-            mimeType: "text; charset=utf-8",
-            json: false
-        })
-    }
+  async getMetadata(
+    _contractAbstraction: ContractAbstraction<ContractProvider | Wallet>,
+    { location }: Tzip16Uri,
+    _context: Context
+  ): Promise<string> {
+    return this._httpBackend.createRequest<string>({
+      url: `${this._gatewayProtocol}://${this._ipfsGateway}/ipfs/${location.substring(2)}/`,
+      method: 'GET',
+      headers: { 'Content-Type': 'text/plain' },
+      mimeType: 'text; charset=utf-8',
+      json: false,
+    });
+  }
 }
+
